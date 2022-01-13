28f27b96e0491ea9e40c4e3319ef00704fdba1b2
saejinpark <54755633+saejinpark@users.noreply.github.com>
2022-01-13 14:15:20 +0900

    popo
    popo

---
+++ webscraper/python-Challenge-1/main.py
@@ -0,0 +1,34 @@
+"""
+As you can see, the code is broken.
+Create the missing functions, use default arguments.
+Sometimes you have to use 'return' and sometimes you dont.
+Start by creating the functions
+"""
+
+def is_on_list(indays, day_of):
+  return day_of in indays
+def get_x(indays, day_index):
+  return indays[day_index]
+def add_x(indays, day_of):
+  return indays.append(day_of)
+def remove_x(indays, day_of):
+  return indays.remove(day_of)
+# \/\/\/\/\/\/\  DO NOT TOUCH AREA  \/\/\/\/\/\/\ #
+
+days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+
+print("Is Wed on 'days' list?", is_on_list(days, "Wed"))
+
+print("The fourth item in 'days' is:", get_x(days, 3))
+
+add_x(days, "Sat")
+print(days)
+
+remove_x(days, "Mon")
+print(days)
+
+
+# /\/\/\/\/\/\/\ END DO NOT TOUCH AREA /\/\/\/\/\/\/\ #
+
+
+
