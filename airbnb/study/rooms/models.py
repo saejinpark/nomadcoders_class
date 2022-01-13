7cca63d66e9573b63ffecec4341692c992da0176
saejinpark <54755633+saejinpark@users.noreply.github.com>
2022-01-13 16:49:14 +0900

    popo
    popop

--- airbnb/study/rooms/models.py
+++ airbnb/study/rooms/models.py
@@ -1,14 +1,22 @@
 from django.db import models
 from django_countries.fields import CountryField
 from core import models as core_models
+from users import models as user_models
 
 # Create your models here.
 class Room(core_models.TimeStampedModel):
     
-    name = models.ChaeField(max_lenght=140)
+    name = models.ChaeField(max_length=140)
     description = models.TextField()
     country = CountryField()
     city = models.CharField(max_length=80)
     price = models.IntegerField()
-    address = model.CharField(max_length=140)
+    address = models.CharField(max_length=140)
+    guests = models.IntegerField()
     bed = models.IntergerField()
+    bedrooms = models.IntergerField()
+    baths = models.IntergerField()
+    check_in = models.TimeField()
+    check_out = models.TimeField()
+    instant_book = models.BooleanField(default=False)
+    host = models.ForeignKey(user_models.User, on_delete=models.CASECADE)
