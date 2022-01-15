d3254948a85b2eebab904c0ce5b5c3174dc446da
saejinpark <54755633+saejinpark@users.noreply.github.com>
2022-01-15 19:16:26 +0900

    1212
    1212

--- airbnb/study/core/models.py
+++ airbnb/study/core/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
 class TimeStampedModel(models.Model):
-    
-    created = models.DateTimeField();
-    updated = models.DateTimeField();
+
+    """ Time Stamped Model """
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
     class Meta:
         abstract = True
