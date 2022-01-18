2742a361bd316642269ab69dc65c50729b7a833a
saejinpark <54755633+saejinpark@users.noreply.github.com>
2022-01-18 17:01:34 +0900

    opopo
    popo

--- Youtube/study/src/controllers/videoController.js
+++ Youtube/study/src/controllers/videoController.js
@@ -10,23 +10,31 @@ Video.find({}, (error, videos) => {
 export const home = async (req, res) => {
   try {
     const videos = await Video.find({});
-    console.log(videos)
+    console.log(videos);
     return res.render("home", { pageTitle: "Home", videos });
   } catch {
     return res.render("server-error", { error });
   }
 };
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
   const { id } = req.params;
-  return res.render("watch", { pageTitle: `Watching` });
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not find!" });
+  }
+  return res.render("watch", { pageTitle: video.title, video });
 };
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
   const { id } = req.params;
-  return res.render("edit", { pageTitle: `Editing` });
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not find!" });
+  }
+  return res.render("edit", { pageTitle: `Editing`, video });
 };
 export const postEdit = (req, res) => {
   const { id } = req.params;
-  const { title } = req.body;
+  console.log(req.body);
   return res.redirect(`/videos/${id}`);
 };
 
@@ -36,16 +44,22 @@ export const getUpload = (req, res) => {
 
 export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
-  const video = new Video({
-    title: title,
-    description: description,
-    createdAt: Date.now(),
-    hashtags: hashtags.split(",").map((word) => `#${word}`),
-    meta: {
-      view: 0,
-      rating: 0,
-    },
-  });
-  await video.save();
-  return res.redirect("/");
+  try {
+    await Video.create({
+      title,
+      description,
+      hashtags: hashtags.split(",").map((word) => `#${word}`),
+      meta: {
+        view: 0,
+        rating: 0,
+      },
+    });
+    return res.redirect("/");
+  } catch (error) {
+    console.log(error);
+    return res.render("upload", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
 };
