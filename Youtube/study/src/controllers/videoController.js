20ae327ae9498424d3ad89cbb692f5108e2868d3
saejinpark <54755633+saejinpark@users.noreply.github.com>
2022-02-08 17:45:16 +0900

    popo
    poppo

--- Youtube/study/src/controllers/videoController.js
+++ Youtube/study/src/controllers/videoController.js
@@ -29,7 +29,7 @@ export const postEdit = async (req, res) => {
   const { title, description, hashtags } = req.body;
   const video = await Video.findById(id);
   if (!video) {
-    return res.render("404", { pageTitle: "Video not find!" });
+    return res.status(404).render("404", { pageTitle: "Video not find!" });
   }
   await Video.findByIdAndUpdate(id, {
     title,
