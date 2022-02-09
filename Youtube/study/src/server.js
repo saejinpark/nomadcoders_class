914eaf37257b9d0fb8df423c03d656c706b3fc9c
saejinpark <54755633+saejinpark@users.noreply.github.com>
2022-02-09 19:01:48 +0900

    Update server.js

--- Youtube/study/src/server.js
+++ Youtube/study/src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
-import MongoStore from "connect-mongo"
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
@@ -18,9 +18,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
     secret: "Hello!",
-    resave: true,
-    saveUninitialized: true,
-    sotre: MongoStore.create({mongoUrl: "mongodb://127.0.0.1:27017/wetube"})
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 20000,
+    },
+    store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/wetube" }),
   })
 );
 
